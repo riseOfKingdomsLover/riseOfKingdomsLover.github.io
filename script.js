3619f09d8dd9698a282e49ebb366cb104375e01a
riseOfKingdomsLover <145271379+riseOfKingdomsLover@users.noreply.github.com>
2023-09-17 17:22:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openDc()
+{
+    window.open("https://link-hub.net/449972/dc-channel", "_blank");
+}
+
+function openTalentCreation()
+{
+    window.open("https://roktalents.com", "_blank");
+}
