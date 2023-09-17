89808c782b229c6b3f2548b4baa5b7773f02e6ee
riseOfKingdomsLover <145271379+riseOfKingdomsLover@users.noreply.github.com>
2023-09-17 18:47:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,8 @@ function openCalculator()
     window.open("https://link-center.net/449972/rok-calculator", "_blank");
 }
 
+function openGuides()
+{
+    window.open("https://link-center.net/449972/guides", "_blank");
+}
+
